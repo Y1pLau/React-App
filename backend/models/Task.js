f757c738895086443964927fb9b66b98d47f546d
Y1pLau <sam.846610@gmail.com>
2025-07-20 21:31:37 +0800

    Task add userID

--- backend/models/Task.js
+++ backend/models/Task.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const TaskSchema = new mongoose.Schema({
   title: { type: String, required: true },
   dueDate: { type: String, required: true },
-  isDone: { type: Boolean, default: true },
+  isDone: { type: Boolean, default: false },
+  userID: { type: mongoose.Schema.Types.ObjectId,  ref: 'User', required: true },
 });
 
 module.exports = mongoose.model('Task', TaskSchema);
