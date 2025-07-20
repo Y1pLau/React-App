ef55af205e48b5d778c012669c96e205c2c7a474
Y1pLau <sam.846610@gmail.com>
2025-07-20 21:29:51 +0800

    del useless dependy

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,10 @@
 // server.js (CommonJS)
 const express = require('express');
 const mongoose = require('mongoose');
-const dotenv = require('dotenv');
 const userRoutes = require('./routes/userRoutes');
 const taskRoutes = require('./routes/taskRoutes');
 const protectedRoute= require('./routes/protectedRoute');
 const cors = require('cors');
-dotenv.config();
 const app = express();
 app.use(cors({
   origin: 'http://localhost:5173', 
